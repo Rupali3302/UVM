045b92705844e236bbea850ae93114197c38c467
Rupali3302 <rupalitirumalasetty@gmail.com>
2025-09-10 13:15:32 +0530

    mux_sequencer.sv

---
+++ mux_testbench/mux_sequencer.sv
@@ -0,0 +1,10 @@
+class mux_sequencer extends uvm_sequencer#(mux_sequence_item);
+  
+  `uvm_component_utils(mux_sequencer)  
+
+  function new(string name="",uvm_component parent);  
+    super.new(name,parent);
+  endfunction
+ 
+endclass
+
